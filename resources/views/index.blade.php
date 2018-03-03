37bc848958898f58f6048731f493531476fe8bb2
Calvin <qelvyn@gmail.com>
2018-03-04 01:33:16 +0700

    Working Files

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,14 +8,15 @@
 	<title>SPESys</title>
 
 	{{--  JQuery  --}}
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="{{asset('../vendor/jquery/jquery-3.3.1.min.js')}}"></script>
+	{{--  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>  --}}
 	<link rel="icon" type="image/png" href="{{asset('assets/images/icons/SPESys-LogoB-64x64.png')}}" />
-	<link rel="stylesheet" type="text/css" href="{{asset('../vendor/bootstrap/css/bootstrap.min.css')}}">
-	{{--  <link rel="stylesheet" type="text/css" href="{{asset('assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{asset('assets/fonts/Linearicons-Free-v1.0.0/icon-font.min.css')}}">  --}}
-	<link rel="stylesheet" type="text/css" href="{{asset('../vendor/animate/animate.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{asset('../vendor/css-hamburgers/hamburgers.min.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{asset('../vendor/select2/select2.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('assets/fonts/Linearicons-Free-v1.0.0/icon-font.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('vendor/animate/animate.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('vendor/css-hamburgers/hamburgers.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('vendor/select2/select2.min.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{asset('css/util.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{asset('css/main.css')}}">
 </head>
@@ -66,12 +67,11 @@
 		</div>
 	</div>
 
-
-
-	<script src="{{('../vendor/bootstrap/js/popper.js')}}"></script>
-	<script src="{{('../vendor/bootstrap/js/bootstrap.min.js')}}"></script>
-	<script src="{{('../vendor/select2/select2.min.js')}}"></script>
-	<script src="js/main.js"></script>
+	
+	<script src="{{asset('vendor/bootstrap/js/popper.js')}}"></script>
+	<script src="{{asset('vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+	<script src="{{asset('vendor/select2/select2.min.js')}}"></script>
+	<script src="{{asset('js/main.js')}}"></script>
 
 </body>
 
